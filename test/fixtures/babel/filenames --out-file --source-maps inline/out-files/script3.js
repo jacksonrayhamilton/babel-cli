4a9bc592928adb7ecef56ca9f7569ddc17e9437d
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2017-11-26 11:34:23 -0800

    Built from babel at 414f3c731.

---
+++ test/fixtures/babel/filenames --out-file --source-maps inline/out-files/script3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Test = function Test() {
+  _classCallCheck(this, Test);
+};
+"use strict";
+
+arr.map(function (x) {
+  return x * MULTIPLIER;
+});
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyIsInNjcmlwdDIuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztJQUFNLEk7Ozs7O0FDQU4sSUFBSSxHQUFKLENBQVE7QUFBQSxTQUFLLElBQUksVUFBVDtBQUFBLENBQVIiLCJmaWxlIjoic2NyaXB0My5qcyIsInNvdXJjZXNDb250ZW50IjpbImNsYXNzIFRlc3Qge1xuXG59IiwiYXJyLm1hcCh4ID0+IHggKiBNVUxUSVBMSUVSKTsiXX0=
